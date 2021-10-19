2c285b3cad3fe48b536690f198b0104f1f701073
Sanjeeb Rao <dev.sanjeeb.rao@gmail.com>
2021-10-19 22:01:44 +0530

    added register and unregister method
    handled invalid class and custom obj

--- src/Parsers/ParserFactory.php
+++ src/Parsers/ParserFactory.php
@@ -5,9 +5,17 @@ namespace SR\Config\Parsers;
 class ParserFactory
 {
     /**
+     * Collection of custom parser.
+     *
+     * @var array
+     */
+    protected static $parser = [];
+
+    /**
      * private construct to stop object creation
      */
-    private function __construct() {
+    private function __construct() 
+    {
     }
 
     /**
@@ -15,7 +23,8 @@ class ParserFactory
      *
      * @return void
      */
-    private function __clone() {
+    private function __clone() 
+    {
     }
     
     /**
@@ -23,9 +32,46 @@ class ParserFactory
      *
      * @param string $type
      * @return Parser
+     * 
+     * @throws InvalidArgumentException
      */
-    public static function getParser(string $type): Parser {
+    public static function getParser(string $type): Parser 
+    {
+        if (isset(static::$parser[$type])) {
+            return static::$parser[$type];
+        }
+
         $parserClass = __NAMESPACE__ . '\\Types\\' .  ucwords(trim($type));
+
+        if (!class_exists($parserClass)) {
+            throw new \InvalidArgumentException("$type type is not supported.");
+        }
+
         return new $parserClass();
+    }
+
+    /**
+     * Register custom parser
+     *
+     * @param string $type
+     * @param Parser $parser
+     * @return void
+     */
+    public static function register(string $type, Parser $parser)
+    {
+        static::$parser[$type] = $parser;
+    }
+
+    /**
+     * Unregister custom parser
+     *
+     * @param string $type
+     * @return boolean
+     */
+    public static function unregister(string $type): bool 
+    {
+        unset(static::$parser[$type]);
+        
+        return true;
     }
 }
